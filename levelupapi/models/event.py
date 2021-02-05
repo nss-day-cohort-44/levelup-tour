559128bfaf625094d86cf58257df88d176056d21
Steve Brownlee <chortlehoort@gmail.com>
2021-02-05 12:32:21 -0600

    Initial migration

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -6,3 +6,4 @@ class Event(models.Model):
     scheduler = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     game = models.ForeignKey("Games", on_delete=models.CASCADE)
     event_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    location = models.CharField(max_length=50)
--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -5,4 +5,6 @@ class Games(models.Model):
 
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
-    name = models.CharField(max_length=75)
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=255)
+    number_of_players = models.IntegerField()
