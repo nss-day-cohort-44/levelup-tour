295afc7d514860adb9f4820c0338b9b8bffc2969
Madi Peper <madisonpeper@gmail.com>
2021-02-11 15:52:14 -0600

    Completed live code of chapter 8; Added viewset for events; Added /event route for server;

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,11 +17,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from levelupapi.views import register_user, login_user
-from levelupapi.views import GameTypes, GamesView
+from levelupapi.views import GameTypes, GamesView, Events
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', GamesView, 'game')
+router.register(r'events', Events, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
